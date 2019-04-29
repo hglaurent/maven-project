5a54963cad2a6323697a5a0b7b695cf19a02f4a2
hglaurent <31436784+hglaurent@users.noreply.github.com>
2019-04-29 14:05:36 +0200

    Update Greeter.java
    By Laurentgh

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -21,6 +21,7 @@ public class Greeter {
    * @return someone string
    */
   public final String greet(final String someone) {
+    System.out.println("Hello");
     return String.format("Hello World, %s!", someone);
   }
 }
